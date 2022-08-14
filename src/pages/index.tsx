926b92a5d338ad86a0cf39262de78967d340e10c
heihei <raions.seibu@gmail.com>
2022-08-14 17:27:06 +0900

    ボタンクリックでランダムに画像を切り替える

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,34 @@
-const IndexPage = () => {
+import { useState } from "react"
+
+const catImages: string[] = [
+    "https://cdn2.thecatapi.com/images/bpc.jpg",
+    "https://cdn2.thecatapi.com/images/eac.jpg",
+    "https://cdn2.thecatapi.com/images/6qi.jpg",
+]
+
+const randomCatImage = ():string => {
+    const index = Math.floor(Math.random() * catImages.length)
     // 猫画像を表示
-    return <img src="https://cdn2.thecatapi.com/images/bpc.jpg" />
+    return catImages[index]
+}
+
+const IndexPage = () => {
+    const [catImageUrl, setCatImageUrl] = useState(
+        "https://cdn2.thecatapi.com/images/bpc.jpg"
+    )
+
+    const handleClick = () => {
+        setCatImageUrl(randomCatImage())
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>今日の猫</button>
+            <div style={{ marginTop: 8}}>
+                <img src={catImageUrl} />
+            </div>
+        </div>
+    )
 }
 
 export default IndexPage
